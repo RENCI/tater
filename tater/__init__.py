bf028021c21481243ff677bff593721cc52c08b1
David Borland <borland@renci.org>
2026-03-04 16:10:55 -0500

    Remove items from __init__.py

--- tater/__init__.py
+++ tater/__init__.py
@@ -1,13 +1,9 @@
 """Tater 2.0: Pydantic-First Document Annotation System."""
-from tater.ui.tater_app import TaterApp
-from tater.ui.cli import parse_args
 from tater.models.span import SpanAnnotation
 from tater.widgets.span import SpanAnnotationWidget, EntityType
 from tater.loaders import load_schema, parse_schema, widgets_from_model
 
 __all__ = [
-    "TaterApp",
-    "parse_args",
     "SpanAnnotation",
     "SpanAnnotationWidget",
     "EntityType",
